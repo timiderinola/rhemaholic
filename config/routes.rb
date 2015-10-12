104f1523885cb609ed869e8836044e38ae0b6bc5
Timilehin Aderinola <timiderinola@gmail.com>
2015-10-12 14:20:08 +0100

    Signup working, with SSL Added in production

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#home'
 
-  # get '/signup', to: 'users#new'
+  get 'signup', to: 'users#new', as: 'signup'
   
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
