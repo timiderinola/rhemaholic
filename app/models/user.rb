2c714b547a2a04dd516536c66fe1faf15f0b4542
Timilehin Aderinola <timiderinola@gmail.com>
2015-10-15 08:43:13 +0100

    Made some minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, :on => :create
+  validates :password, length: { minimum: 6 }, :on => :create
   validates :password_confirmation, presence: true, :on => :create
 
   def following?(leader)
