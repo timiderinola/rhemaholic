e3c308d7a4dce054ede7cfabb47acbd5ab023673
Timilehin Aderinola <timiderinola@gmail.com>
2015-10-29 22:10:22 +0100

    Added help page, testing

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,33 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
+class StaticPagesControllerTest < ActionController::TestCase
 
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
+  test "should get home" do
+    get :home
+    assert_response :success
   end
 
-  describe "GET #help" do
-    it "returns http success" do
-      get :help
-      expect(response).to have_http_status(:success)
-    end
+  test "should get help" do
+    get :help
+    assert_response :success
   end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #contact" do
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
