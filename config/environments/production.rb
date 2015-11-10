e3390dcbe7d3237627ec1b4d699166f8315e2db2
Timilehin Aderinola <timiderinola@gmail.com>
2015-11-10 19:16:53 +0100

    Fixed an error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+rhemaholic::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action.mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   host = 'rhemaholic.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
