9328a022576d3c55762035439ffd2a31f51df8fa
Timilehin Aderinola <timiderinola@gmail.com>
2015-11-16 02:53:37 +0100

    Changed routes and home page structure.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    redirect_to signin_path unless signed_in?
     @micropost = current_user.microposts.build if signed_in?
     @feed_items = current_user.feed.paginate(page: params[:page]) if signed_in?
   end
