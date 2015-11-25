449a7e0acb1dc388598db9bed3aa4433b97194d6
Timilehin Aderinola <timiderinola@gmail.com>
2015-11-25 11:39:40 +0100

    Edited application layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,10 +83,6 @@ class User < ActiveRecord::Base
     if leader != self && following?(leader)
       leaders.delete(leader)
     end
-  end 
-
-  def timeline_user_ids
-    leader_ids + [id]
   end
 
   def feed
