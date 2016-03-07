ef92c56477ddbb96658cdcd5e8f1f57f52d36fcd
Timilehin Aderinola <timiderinola@gmail.com>
2016-03-07 20:59:21 +0100

    Fixing heroku issue

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC')}
-  mount_uploader :picture, PictureUploader
+  # mount_uploader :picture, PictureUploader
 
   validates :user_id, presence: true
   validates :title, presence: true
