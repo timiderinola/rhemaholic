d3ebcdd120a07108dd4d64ff3aea46f0a1a58abd
Bayode Aderinola <bayodesegun@gmail.com>
2016-10-03 15:02:22 +0100

    autofill name and email for signed_in user on contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,10 @@
 class ContactsController < ApplicationController
+  before_filter :get_user
+
+  def get_user
+      @user = current_user if signed_in?
+  end
+  
   def new
     @contact = Contact.new
   end
